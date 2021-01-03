33bd862a4dd82c0a1c2a1c1a4317a4d1862a1778
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-01-03 18:25:24 +0200

    database and clearing stuff

---
+++ src/database.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
+};
+
+const database = firebase.initializeApp(config);
+export default database;
